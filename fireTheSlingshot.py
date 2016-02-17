0b4f32cff505256e786068388ba71f5f7a83c6a2
Sam Weiller <sam.weiller@gmail.com>
2016-02-17 13:48:05 -0500

    Add fireTheSlingshot including basic plan and reqs

---
+++ fireTheSlingshot.py
@@ -0,0 +1,23 @@
+# fireTheSlingshot.py
+
+# STAGES
+# For TV:
+# Compare home dirs to remote dirs (searching for existing shows)
+# If show exists, do same for subfolder (search for existing seasons)
+# if Season exists, check FILES for existing episodes
+# Copy only new files to appropriate directories on server
+# move copied files to ROCKET folder
+
+# For Movies:
+# Compare home files to remote files (search for existing movies)
+# Copy only new files to appropriate directories on server
+# Move copied files to ROCKET folder
+
+# At End:
+# Ask if user wants to fire the rocket (delete copied files)
+
+
+# REQUIREMENTS
+# Feedback for user throughout
+# NO login each time (known SSH key)
+# Error handling
